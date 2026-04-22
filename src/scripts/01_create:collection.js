5f5bdc13d0a9e936d97108055397dcdb3485060f
Lopez-Torres-Erick-de-Jesus <lopez.torres.erick.de.jesus.m3s2@gmail.com>
2026-04-22 12:31:57 -0600

    Update 01_create:collection.js

--- src/scripts/01_create:collection.js
+++ src/scripts/01_create:collection.js
@@ -1,1 +1,26 @@
-
+[
+  {
+    "nombre": "Usuario1",
+    "username": "user1",
+    "edad": 22,
+    "correo": "user1@email.com",
+    "genero": "Femenino",
+    "perfil_musical": {
+      "generos_favoritos": ["Pop", "Rock"],
+      "artistas_favoritos": ["The Weeknd", "Queen"],
+      "nivel_actividad": 0.65
+    }
+  },
+  {
+    "nombre": "Usuario2",
+    "username": "user2",
+    "edad": 25,
+    "correo": "user2@email.com",
+    "genero": "Masculino",
+    "perfil_musical": {
+      "generos_favoritos": ["Reggaeton", "Trap"],
+      "artistas_favoritos": ["Bad Bunny"],
+      "nivel_actividad": 0.82
+    }
+  }
+]
